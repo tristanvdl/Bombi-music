a25c068d14427158ef7b1fa3061aa423e9878bea
Tristan van der Linden <tristanvdl@gmail.com>
2016-11-02 09:48:08 +0100

    added about and lessen page

---
+++ page-about.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: trist
+ * Date: 26-10-2016
+ * Time: 14:55
+ */
+get_header();
+?>
+    <div class="container">
+        <div class="row wrapper_about_text">
+            <div class="col-md-6 text_about">
+                <h2 class="Overmij">OVER MIJ</h2>
+                <p class="tekst1">Ik ben opgegroeid met veel muziek in huis. Op mijn 4e begon ik met vioolles en toen ik
+                    9 was ruilde ik de viool in voor de klarinet.
+                    Ik ben tijdens mijn studie gaan werken in het Muziekgebouw aan ‘t IJ waar ik workshops geef in de
+                    ‘Klankspeeltuin’.
+                    <br><br>
+                    In 2014 heb ik mijn Bachelor of Music behaald aan het conservatorium in Haarlem. Omdat ik lesgeven
+                    altijd het leukst heb gevonden wilde ik graag nog mijn lesbevoegdheid halen. Ik ging door met de 2
+                    jarige opleiding docent muziek als zij-instromer aan het conservatorium van Amsterdam. In 2016 hoop
+                    ik mijn Bachelor of Music in Education in ontvangst te mogen nemen. De combinatie studeren en werken
+                    is erg leuk maar ik kan niet wachten om full time aan de slag te gaan. </p>
+            </div>
+            <div class="col-md-5">
+                <img class="foto" src="<?php echo get_bloginfo('template_url') ?>/images/portrettransgroot.png"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="fotocontainer">
+                <div class="col-md-2">
+                   <img class="foto1" src="<?php echo get_bloginfo('template_url') ?>/images/irene1.jpg"/>
+                </div>
+                <div class="col-md-2">
+                    <img class="foto1" src="<?php echo get_bloginfo('template_url') ?>/images/irene2.jpg"/>
+                </div>
+                <div class="col-md-2">
+                    <img class="foto1" src="<?php echo get_bloginfo('template_url') ?>/images/irene3.jpg"/>
+                </div>
+                <div class="col-md-2">
+                    <img class="foto1" src="<?php echo get_bloginfo('template_url') ?>/images/irene4.jpg"/>
+                </div>
+                <div class="col-md-2">
+                    <img class="foto1" src="<?php echo get_bloginfo('template_url') ?>/images/irene5.jpg"/>
+                </div>
+                <div class="col-md-2">
+                    <img class="foto1" src="<?php echo get_bloginfo('template_url') ?>/images/irene6.jpg"/>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
---
+++ page-lessen.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: trist
+ * Date: 26-10-2016
+ * Time: 14:55
+ */
+get_header();
+?>
+
+<!-- Custom Fonts -->
+<img class="headerimage" src="<?php echo get_bloginfo('template_url') ?>/images/headerimage.jpg"/>
+<div class="container ">
+    <div class="achtergrondtekst">
+        <h1 class="lessen">Lessen</h1>
+        <p class="lessentekst paddingtekst">Wat ben ik blij dat ik van jongs af aan met muziek bezig ben geweest. En wat
+            is
+            het fijn om dit over te brengen op andere kinderen. Maar ook op volwassenen, het is nooit te laat om te
+            leren of
+            om een oude hobby weer op te pakken!<br><br> Noten lezen leren kinderen erg snel. Ik vind het belangrijk om
+            kinderen een goede basis aan te bieden zodat ze later zelf kunnen bepalen welke richting ze op kunnen gaan.
+            Naast het leren lezen van noten worden bij piano- en gitaarles akkoorden behandeld. Met akkoorden kunnen ze
+            op
+            een snelle manier al veel liedjes spelen en op deze manier wordt er ook veel gezongen in de les.<br><br>
+            Voor
+            jonge leerlingen maak ik af en toe een spel of quiz waarbij ze hun kennis moeten toepassen en leuke
+            opdrachtjes
+            doen. Ook is het erg motiverend om de leerling zelf liedjes te laten kiezen. Op het niveau van de leerling
+            gaan
+            we aan de slag met een eigen gekozen nummer. Naast het ‘componeren’ van een eigen liedje komt het uit het
+            hoofd
+            spelen en op gehoor iets naspelen ook aan bod. Zo hoop ik een goede muzikale basis te leggen en mijn
+            enthousiasme voor muziek over te brengen.<br><br> Voor volwassenen richt ik mij vooral op wat ze zelf willen
+            leren. Samen zoeken we naar een manier waarop we de lessen kunnen indelen zodat het plezier in muziek maken
+            blijft.
+        </p>
+    </div>
+
+    <div class="row wrapper_pics_music achtergrond">
+        <div class="col-sm-4">
+            <img class="muziek" src="<?php echo get_bloginfo('template_url') ?>/images/piano.jpg"/>
+            <h3 class="kopjeslessen">Piano</h3>
+            <p class="tekstlessen paddingtekst">Bij de pianolessen wordt een methode gezocht die bij de leerling past.
+                Hierbij is noten leren lezen en de vingerzetting een basis. Daarnaast werk ik met akkoorden, een manier
+                om op een snelle manier liedjes te kunnen begeleiden. Ook worden er liedjes naar wens van de leerling
+                gespeeld die op niveau worden uitgewerkt.</p>
+        </div>
+        <div class="col-sm-4">
+            <img class="muziek1" src="<?php echo get_bloginfo('template_url') ?>/images/klarinet.jpg"/>
+            <h3 class="kopjeslessen">Klarinet</h3>
+            <p class="tekstlessen paddingtekst">Tijdens de klarinetles wordt het boek ‘Horen, Lezen, en Spelen’
+                gebruikt. Noten lezen, ritme- en luisteroefeningen komen
+                aan bod. Daarnaast sta ik open voor input van de leerling en maak ik zelf materiaal op niveau en wens
+                van de leerling.</p>
+        </div>
+        <div class="col-sm-4">
+            <img class="muziek2" src="<?php echo get_bloginfo('template_url') ?>/images/gitaar.jpg"/>
+            <h3 class="kopjeslessen">Gitaar</h3>
+            <p class="tekstlessen paddingtekst">Bij de gitaarlessen maak ik gebruik van de methode ‘(De kleine)
+                Gitaarstarter’. Met deze methode maken ze kennis met het notenschrift. Daarnaast maak ik gebruik van de
+                methode ‘Slaggitaar’, een methode waarin akkoorden en slagritmes worden behandeld. Naast het werken uit
+                de boeken kijk ik naar de wensen van
+                de leerling en geef ik materiaal van nummers die de leerling graag zou willen spelen.</p>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
+
+
